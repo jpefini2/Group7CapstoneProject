2c30a24998067c9926983050451c1f1359f82d90
Darrius Dupree <darriusdupree@gmail.com>
2021-02-08 18:04:12 -0500

    Added an empty test
    Added an empty test. Only commiting because I can't pull the recent changes without commiting current stuff

---
+++ code/AdvisementManager/AdvisementManagerDesktopUnitTesting/Controller/AdvisementSessionsController/RemoveHoldTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AdvisementManagerWebApp.Models;
+using AdvisementManagerWebAppUnitTesting.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace AdvisementManagerWebAppUnitTesting.Controller.AdvisementSessionsController
+{
+    [TestClass]
+    public class RemoveHoldTests
+    {
+        [TestMethod]
+        public void RedirectResultTest()
+        {
+
+        }
+    }
+}
