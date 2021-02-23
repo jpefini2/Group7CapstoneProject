01d631225350f742122a60710d40b06a4b897580
Justin McConnell <jmcconn1@my.westga.edu>
2021-02-23 13:44:44 -0500

    Added loginDAL tests
    Added test cases to test succesful password, unsuccessful password, and
    empty

--- code/AdvisementManager/AdvisementManagerDesktopUnitTesting/DAL/LoginDAL/LoginDALTests.cs
+++ code/AdvisementManager/AdvisementManagerDesktopUnitTesting/DAL/LoginDAL/LoginDALTests.cs
@@ -1,17 +1,23 @@
 ﻿using AdvisementManagerWebApp.Data;
 using AdvisementManagerWebApp.Models;
+using AdvisementManagerWebApp.DAL;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
 namespace AdvisementManagerWebAppUnitTesting.DAL.LoginDAL
 {
     [TestClass]
-    class LoginDALTests
+    public class LoginDALTests
     {
-        [TestMethod]
-        public void LoginTest()
+        Student student;
+        Advisor advisor;
+        User user;
+
+        [TestInitialize]
+        public void Setup()
         {
-            var student = new Student
+            student = new Student
             {
                 Id = 0,
                 FirstName = "Craig",
@@ -21,7 +27,7 @@ namespace AdvisementManagerWebAppUnitTesting.DAL.LoginDAL
                 facultyAdvisorId = 0
             };
 
-            var advisor = new Advisor
+            advisor = new Advisor
             {
                 Id = 0,
                 FirstName = "Gina",
@@ -31,14 +37,108 @@ namespace AdvisementManagerWebAppUnitTesting.DAL.LoginDAL
                 UserName = "gsmith"
             };
 
-            Mock<ApplicationDbContext> context = new Mock<ApplicationDbContext>();
+            user = new User
+            {
+                Username = "gsmith",
+                PasswordHash = "$2a$12$X552DMehlH2hKatG8SR8gOqsBt6O20eUZ0vglH0h/ZPr9sSI5z96S"
+            };
 
-            context.Object.Add(student);
-            context.Object.Add(advisor);
 
-            var loginDAL = new AdvisementManagerWebApp.DAL.LoginDAL(context.Object);
-            //bool success = loginDAL.AttemptLogin("gsmith", "password1");
-            //Assert.IsTrue(success);
+        }
+        [TestMethod]
+        public void LoginDALCorrectPasswordTest()
+        {
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(databaseName: "AdvisementManagement")
+                          .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Add(student);
+                context.Add(advisor);
+                context.Add(user);
+                context.SaveChanges();
+
+                string correctpassword = "password1";
+
+                var loginDAL = new AdvisementManagerWebApp.DAL.LoginDAL(context);
+                string hash = loginDAL.AttemptLogin("gsmith", correctpassword);
+                if (string.IsNullOrEmpty(hash))
+                {
+                    throw new AssertFailedException();
+                }
+                Assert.IsTrue(BCrypt.Net.BCrypt.Verify(correctpassword, user.PasswordHash));
+            }
+        }
+
+        [TestMethod]
+        public void LoginDALIncorrectPasswordTest()
+        {
+
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(databaseName: "AdvisementManagement")
+                          .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Add(student);
+                context.Add(advisor);
+                context.Add(user);
+                context.SaveChanges();
+
+                string wrongpassword = "ThisPasswordIsWrong";
+
+                var loginDAL = new AdvisementManagerWebApp.DAL.LoginDAL(context);
+                string hash = loginDAL.AttemptLogin("gsmith", wrongpassword);
+                Assert.IsNull(hash);
+                Assert.IsFalse(BCrypt.Net.BCrypt.Verify(wrongpassword, user.PasswordHash));
+            }
+        }
+
+        [TestMethod]
+        public void LoginDALEmptyPasswordTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(databaseName: "AdvisementManagement")
+                          .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Add(student);
+                context.Add(advisor);
+                context.Add(user);
+                context.SaveChanges();
+
+                string inputpassword = "";
+
+                var loginDAL = new AdvisementManagerWebApp.DAL.LoginDAL(context);
+                string hash = loginDAL.AttemptLogin("gsmith", inputpassword);
+                Assert.IsNull(hash);
+                Assert.IsFalse(BCrypt.Net.BCrypt.Verify(inputpassword, user.PasswordHash));
+            }
+        }
+
+        [TestMethod]
+        public void LoginDALEmptyUserTest()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                          .UseInMemoryDatabase(databaseName: "AdvisementManagement")
+                          .Options;
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.Add(student);
+                context.Add(advisor);
+                context.Add(user);
+                context.SaveChanges();
+
+                string inputpassword = "password1";
+
+                var loginDAL = new AdvisementManagerWebApp.DAL.LoginDAL(context);
+                string hash = loginDAL.AttemptLogin("", inputpassword);
+                Assert.IsNull(hash);
+            }
         }
     }
 }
---
+++ code/AdvisementManager/AdvisementManagerWebApp/Resources/CookieManager.cs
@@ -0,0 +1,27 @@
+﻿using AdvisementManagerWebApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AdvisementManagerWebApp.Utility
+{
+    public interface IClientStorageManager
+    {
+        void Store(string key, string value);
+    }
+    public class CookieManager : IClientStorageManager
+    {
+        readonly ApplicationDbContext context;
+        public CookieManager(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public void Store(string key, string value)
+        {
+            //context.GetHttpContext().Response.Cookies.Append(key, value);
+        }
+    }
+}
