45d15920bdb352a92d226d32c98a85ca46cbc64b
Darrius Dupree <darriusdupree@gmail.com>
2021-03-05 08:24:59 -0500

    Created a winForm app and unit test project for it
    Created a win form app with a test class and a unit test project for the win form app with a test calss as well to make sure references to the project can be made without issues.

---
+++ code/AdvisementManager/AdvisementManagerDesktopUnitTests/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using AdvisementDesktop.Model;
+
+namespace AdvisementManagerDesktopUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var hello = "hello";
+            var test = new testModelClass {
+                hello = hello
+            };
+
+            Assert.AreEqual(hello, hello);
+        }
+    }
+}
